fe08de577c9e2831097e7f36cf856c5bbe650595
faisalibal33 <faisaliqbal030397@gmail.com>
2022-09-28 22:41:16 +0700

    add Flatlist item

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   const [text, setText] = useState("");
@@ -10,8 +18,10 @@ export default function App() {
     setText(e);
   };
   const addGoalHandler = () => {
-    setCourseGoals((prev) => [...prev, text]);
-    console.log(courseGoals);
+    setCourseGoals((prev) => [
+      ...prev,
+      { text: text, id: Math.random().toString() },
+    ]);
   };
 
   return (
@@ -25,9 +35,24 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((item, id) => (
-          <Text key={id}>{item}</Text>
-        ))}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.textGoals}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+        {/* <ScrollView>
+          {courseGoals.map((item, id) => (
+            
+          ))}
+        </ScrollView> */}
       </View>
     </View>
   );
@@ -56,6 +81,16 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   goalsContainer: {
-    flex: 4,
+    flex: 5,
+  },
+
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  textGoals: {
+    color: "white",
   },
 });
